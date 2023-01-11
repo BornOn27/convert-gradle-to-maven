5da61f7e01d76575b7b47e8b0d69be89087e588b
Manoj Kumar Singh <manojkumar.singh@livspace.com>
2023-01-11 13:22:24 +0530

    I am getting better everyday

---
+++ src/main/java/com/flodeus/migration/ConvertGradleToMaven.java
@@ -0,0 +1,12 @@
+package com.flodeus.migration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertGradleToMaven {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConvertGradleToMaven.class, args);
+    }
+}
